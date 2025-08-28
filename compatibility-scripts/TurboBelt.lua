b26926c40761f05caa04f769a4cfa3459d6e9aef
UltraMurlock <89306501+UltraMurlock@users.noreply.github.com>
2025-08-28 14:18:37 +0300

    Compatibility: TurboBelt

---
+++ compatibility-scripts/TurboBelt.lua
@@ -0,0 +1,4 @@
+data.raw.recipe["turbo-transport-belt"].category = "pressing"
+data.raw.recipe["turbo-underground-belt"].category = "pressing"
+data.raw.recipe["turbo-splitter"].category = "pressing"
+data.raw.recipe["turbo-loader"].category = "pressing"
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,7 @@
 if mods["quality"] then
     require("compatibility-scripts.quality")
 end
+
+if mods["TurboBelt"] then
+    require("compatibility-scripts.TurboBelt")
+end
